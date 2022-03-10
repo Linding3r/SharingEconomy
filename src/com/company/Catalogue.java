4bc1fb25fbda6198456e1840d4f1a1dd939b8ab2
Linding3r <thomasjuullindinger@yahoo.dk>
2022-03-10 19:51:30 +0100

    full working program with several hardcoded lines,

--- src/com/company/Catalogue.java
+++ src/com/company/Catalogue.java
@@ -2,11 +2,11 @@ package com.company;
 
 public class Catalogue {
 
-  //Create a Class array from Item as a Class is also a datatype
-  private  Item[] catalogue;
+  //Create an array that can hold Item (Item Class)
+  private Item[] catalogue;
 
 
-  public Catalogue(int size){
+  public Catalogue(int size) {
     catalogue = new Item[size];
   }
 
@@ -14,13 +14,44 @@ public class Catalogue {
     return catalogue;
   }
 
-  public void addItem(Item item){
-    for(int x = 0; x < catalogue.length; x++){
-      if (catalogue[x] == null){
+  public Item[] getAvailableItems() {
+    int i = 0;
+    Item[] availableCatalogue = new Item[catalogue.length];
+    for (int x = 0; x < catalogue.length; x++) {
+      if (catalogue[x] == null) {
+        break;
+      }
+      if (catalogue[x].available) {
+        availableCatalogue[i] = catalogue[x];
+        i++;
+      }
+    }
+    return availableCatalogue;
+  }
+
+  public Item findItem(String description) {
+    for (int x = 0; x < catalogue.length; x++) {
+      if (catalogue[x].name.equals(description)) {
+        return catalogue[x];
+      }
+    }
+    return null;
+  }
+
+  public void borrowItem(Item item) {
+    item.setAvailable(false);
+  }
+
+  public void returnItem(Item item) {
+    item.setAvailable(true);
+  }
+
+  public void addItem(Item item) {
+    for (int x = 0; x < catalogue.length; x++) {
+      if (catalogue[x] == null) {
         catalogue[x] = item;
+        return;
       }
     }
   }
-
-
 }
--- src/com/company/Item.java
+++ src/com/company/Item.java
@@ -3,40 +3,46 @@ package com.company;
 public class Item {
 
   public String category;
-  public String object;
+  public String name;
   public boolean available;
 
   public Item(String category, String object){
     this.category = category;
-    this.object = object;
+    this.name = object;
     this.available = true;
-  }
-
-  public void setCategory(String category) {
-    this.category = category;
-  }
-
-  public void setObject(String object) {
-    this.object = object;
   }
 
   public void setAvailable(boolean available) {
     this.available = available;
   }
 
-  public String getCategory() {
-    return category;
-  }
-
-  public String getObject() {
-    return object;
-  }
-
   public boolean isAvailable() {
     return available;
   }
 
+  public String getCategory() {
+    return category;
+  }
+
+  public void setCategory(String category) {
+    this.category = category;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public boolean checkForItem(Item item){
+    if (this.name.equals(item.name)){
+      return true;
+    }return false;
+  }
+
   public String toString(){
-    return category + " " + object + " " + available;
+    return name + " " + category + " " + available;
   }
 }
